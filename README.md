1972506b9aec4010f7d8b36df3538cc3d540ccfe
wulfric012 <rohansahay281@gmail.com>
2024-02-14 00:28:24 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# HooBank Frontend responsive design 
+# NextGenBank Frontend responsive design 
 
-Figma file: https://www.figma.com/file/bUGIPys15E78w9bs1l4tgS/HooBank?type=design&node-id=0-1&mode=design&t=dPUHkmGBww0atuss-0
+Figma file: https://www.figma.com/file/aaYXi4iQvN0eBwLMBnC71a/HooBank-(Copy)?type=design&node-id=0%3A1&mode=design&t=PDQwSWx9wBiqwQht-1
 
 Made using Reactjs, Tailwind CSS!
--- src/constants/index.js
+++ src/constants/index.js
@@ -48,7 +48,7 @@ export const feedback = [
     id: "feedback-1",
     content:
       "Money is only a tool. It will take you wherever you wish, but it will not replace you as the driver.",
-    name: "Herman Jensen",
+    name: "Lily Jensen",
     title: "Founder & Leader",
     img: people01,
   },
@@ -56,7 +56,7 @@ export const feedback = [
     id: "feedback-2",
     content:
       "Money makes your life easier. If you're lucky to have it, you're lucky.",
-    name: "Steve Mark",
+    name: "Steve Smith",
     title: "Founder & Leader",
     img: people02,
   },
@@ -64,7 +64,7 @@ export const feedback = [
     id: "feedback-3",
     content:
       "It is usually people in the money business, finance, and international trade that are really rich.",
-    name: "Kenn Gallagher",
+    name: "Joe Wellington",
     title: "Founder & Leader",
     img: people03,
   },
@@ -94,23 +94,23 @@ export const footerLinks = [
     links: [
       {
         name: "Content",
-        link: "https://www.hoobank.com/content/",
+        link: "https://www.nextgenbank.com/content/",
       },
       {
         name: "How it Works",
-        link: "https://www.hoobank.com/how-it-works/",
+        link: "https://www.nextgenbank.com/how-it-works/",
       },
       {
         name: "Create",
-        link: "https://www.hoobank.com/create/",
+        link: "https://www.nextgenbank.com/create/",
       },
       {
         name: "Explore",
-        link: "https://www.hoobank.com/explore/",
+        link: "https://www.nextgenbank.com/explore/",
       },
       {
         name: "Terms & Services",
-        link: "https://www.hoobank.com/terms-and-services/",
+        link: "https://www.nextgenbank.com/terms-and-services/",
       },
     ],
   },
@@ -119,23 +119,23 @@ export const footerLinks = [
     links: [
       {
         name: "Help Center",
-        link: "https://www.hoobank.com/help-center/",
+        link: "https://www.nextgenbank.com/help-center/",
       },
       {
         name: "Partners",
-        link: "https://www.hoobank.com/partners/",
+        link: "https://www.nextgenbank.com/partners/",
       },
       {
         name: "Suggestions",
-        link: "https://www.hoobank.com/suggestions/",
+        link: "https://www.nextgenbank.com/suggestions/",
       },
       {
         name: "Blog",
-        link: "https://www.hoobank.com/blog/",
+        link: "https://www.nextgenbank.com/blog/",
       },
       {
         name: "Newsletters",
-        link: "https://www.hoobank.com/newsletters/",
+        link: "https://www.nextgenbank.com/newsletters/",
       },
     ],
   },
@@ -144,11 +144,11 @@ export const footerLinks = [
     links: [
       {
         name: "Our Partner",
-        link: "https://www.hoobank.com/our-partner/",
+        link: "https://www.nextgenbank.com/our-partner/",
       },
       {
         name: "Become a Partner",
-        link: "https://www.hoobank.com/become-a-partner/",
+        link: "https://www.nextgenbank.com/become-a-partner/",
       },
     ],
   },
